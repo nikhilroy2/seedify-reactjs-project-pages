d14a822703f76926a988c3f98f1d24b425af9c99
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2023-07-29 12:04:16 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Base from './layout/base/Base';
+// .................Pages............................
+import Homepage from './pages/homepage/Homepage';
+// .................Pages............................End
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Base>
+        <Homepage></Homepage>
+      </Base>
     </div>
   );
 }
