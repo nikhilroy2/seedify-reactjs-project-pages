ea1360aad27b9b8a918d84ff325821b52c9a6f3f
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2023-07-30 15:20:12 +0600

    update header

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,118 +1,108 @@
 import React from 'react';
-
+import './Header.css';
 function Header(props) {
     return (
         <header id='Header'>
-            <nav className="bg-white border-gray-200 dark:bg-gray-900">
-                <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
-                    <a href="https://flowbite.com" className="flex items-center">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                    </a>
-                    <div className="flex items-center md:order-2">
-                        <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</a>
-                        <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</a>
-                        <button data-collapse-toggle="mega-menu" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu" aria-expanded="false">
-                            <span className="sr-only">Open main menu</span>
-                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div id="mega-menu" className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-                        <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
-                            <li>
-                                <a href="#" className="block py-2 pl-3 pr-4 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
-                            </li>
-                            <li>
-                                <button id="mega-menu-dropdown-button" data-dropdown-toggle="mega-menu-dropdown" className="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-900 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
-                                    Company <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 4 4 4-4" />
-                                    </svg>
-                                </button>
-                                <div id="mega-menu-dropdown" className="absolute z-10 grid hidden w-auto grid-cols-2 text-sm bg-white border border-gray-100 rounded-lg shadow-md dark:border-gray-700 md:grid-cols-3 dark:bg-gray-700">
-                                    <div className="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
-                                        <ul className="space-y-4" aria-labelledby="mega-menu-dropdown-button">
-                                            <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                                    About Us
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                                    Library
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                                    Resources
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                                    Pro Version
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div className="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
-                                        <ul className="space-y-4">
-                                            <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                                    Blog
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                                    Newsletter
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                                    Playground
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                                    License
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div className="p-4">
-                                        <ul className="space-y-4">
-                                            <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                                    Contact Us
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                                    Support Center
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                                    Terms
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Team</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-
+            <Navbar></Navbar>
         </header>
     );
 }
 
 export default Header;
+
+
+const Navbar = () => {
+
+    const dropdown_object = {
+        igo_launchpad: [
+            {
+                id: 1,
+                icon: require('../../static/icons/homead17.png'),
+                name: 'IGO Launchpad Home',
+                link: '#',
+                description: 'Learn more about the way the IGOs work at Seedify, and how to join them.'
+            },
+            {
+                id: 2,
+                icon: require('../../static/icons/purchase4aa1.png'),
+                name: 'Purchase SFUND',
+                link: '#',
+                description: 'Acquire our main token and reap the rewards of its utility in our ecosystem.'
+            },
+            {
+                id: 3,
+                icon: require('../../static/icons/claims7606.png'),
+                name: 'Claim',
+                link: '#',
+                description: 'Browse the available claims and see the ones you are eligible for.'
+            },
+            {
+                id: 4,
+                icon: require('../../static/icons/guides56dc.png'),
+                name: 'Guides',
+                link: '#',
+                description: 'Explore our documentation and help articles to guide your quest at Seedify.'
+            },
+        ]
+    }
+    return (
+        <nav id='Navbar'>
+            <div className="flex alilgn-items-center">
+                <div className="item_box">
+                    <a href="#" className='logo'>
+                        <img src={require('../../static/images/logo.c8a4712a.svg').default} alt="" />
+                    </a>
+                </div>
+
+            </div>
+
+            <ul className='item_box flex items-center'>
+                <li className='hover_dropdown'>
+                    <a href="#" className='action_item'>IGO Launchpad</a>
+                    <ul className="hover_dropdown_list">
+                        <li className="dropdown_item">
+                            <ul>
+                                <li>
+                                    <a href="#" className="dropdown_action">
+                                        <div className="flex items-start">
+                                            <img className='me-3' src={require('../../static/icons/homead17.png')} alt="" />
+                                            <div className="colntent">
+                                                <h3 className='text-xl'>IGO Launchpad Home</h3>
+                                                <p className='text-sm'>
+                                                    Learn more about the way the IGOs work at Seedify, and how to join them.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="dropdown_action">
+                                        <div className="flex items-start">
+                                            <img className='me-3' src={require('../../static/icons/homead17.png')} alt="" />
+                                            <div className="colntent">
+                                                <h3 className='text-xl'>Purchase SFUND</h3>
+                                                <p className='text-sm'>
+                                                    Acquire our main token and reap the rewards of its utility in our ecosystem.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" className='action_item'>INO Launchpad</a>
+                </li>
+                <li>
+                    <a href="#" className='action_item'>Stake/Farm</a>
+                </li>
+
+                <li>
+                    <a href="#" className='action_item'>Claims</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
