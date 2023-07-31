a1201157c081216aa073066d8053e325dea55aff
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2023-07-31 10:40:55 +0600

    update

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -5,6 +5,8 @@ function Homepage(props) {
     return (
         <div id='Homepage'>
             <Section1></Section1>
+            <Section2></Section2>
+            <Section3></Section3>
         </div>
     );
 }
@@ -48,7 +50,7 @@ const Section1 = () => {
     return (
         <section id="Section1">
             <div className="section_container">
-                <div className="flex content-between gap-4 items-center">
+                <div className="flex justify-between gap-4 items-center">
                     <div className="content_box mt-12 max-w-xl">
                         <h1 className='text-5xl font-bold'>
                             Join Top-Tier AI, Web3 Gaming & Metaverse IDOs With <span className='text-[#7bf5fb]'>Seedify</span>
@@ -67,7 +69,7 @@ const Section1 = () => {
                         </div>
 
                         <div className="company_wrapper mt-12">
-                            <h4 style={{opacity: .6}} className='font-bold'>AVAILABLE ON:</h4>
+                            <h4 style={{ opacity: .6 }} className='font-bold'>AVAILABLE ON:</h4>
                             <ul className='flex items-center gap-3'>
                                 {company_images.map((v) => {
                                     return (
@@ -95,3 +97,58 @@ const Section1 = () => {
 }
 
 
+const Section2 = () => {
+    return (
+        <section id="Section2">
+            <div className="section_container">
+                <div className="content_wrapper">
+                    <h2 className='font-bold text-4xl section_title'>
+                        Upcoming Pools
+                    </h2>
+                    <a href='#' className="pool_card block">
+                        <div className="card_header flex  gap-6 p-3">
+                            <img src={require('../../static/images/1688740172997solidus-logo.webp')} alt="img" />
+                            <div className="content">
+                                <div className="flex items-start mb-3 justify-between"><h3>Solidus AI</h3>
+                                    <a href="#" className="igo_action">IGO</a></div>
+                                <p>
+                                    Solidus has successfully constructed an environmentally conscious, state-of-the-art High-Performance Computing (HPC) Data Centre spanning over 8,000 square feet. This facility is strategically situated in a highly secure European location. In conjunction with this remarkable achievement, Solidus is poised to unveil the ...
+                                </p>
+                            </div>
+                        </div>
+                        <div className="card_body p-3">
+                            <div className="flex justify-between gap-2">
+                                <div className="item_box">
+                                    <span className="block content">MIN. ALLOCATION</span>
+                                    <span className="block value">0.01 BUSD</span>
+                                </div>
+                                <div className="item_box">
+                                    <span className="block content">MAXIMUM</span>
+                                    <span className="block value">0.01</span>
+                                </div>
+                                <div className="item_box">
+                                    <span className="block content">ACCESS</span>
+                                    <span className="block value">Public</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card_footer"></div>
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+const Section3 = () => {
+    return (
+        <section id="Section3">
+            <div className="section_container">
+                <h2 className='font-bold text-4xl section_title text-center'>
+                    How To Buy SFUND
+                </h2>
+            </div>
+        </section>
+    )
+}
