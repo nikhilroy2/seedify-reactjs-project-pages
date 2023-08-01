3faf859c541cd382c1d087f3c8467c2cff49ae90
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2023-08-01 17:01:57 +0600

    update

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -274,13 +274,154 @@ const Section5 = () => {
             <div className="section_container">
                 <div className="section_inner">
                     <div className="item_box">
-                        <h2>
-                            Frequently Asked Questions
-                        </h2>
-                        <p>
-                            Haven't found the answers you are looking for? Contact us at
-                            <span>support@seedify.com</span>
-                        </p>
+                        <div className="content_header">
+                            <h2 className='text-[48px] font-bold mb-[17px]'>
+                                Frequently Asked Questions
+                            </h2>
+                            <p>
+                                Haven't found the answers you are looking for? Contact us at <br />
+                                <span>support@seedify.com</span>
+                            </p>
+                        </div>
+                        <div className="content_body">
+                            <div>
+                                <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
+                                    <ul className="tab_list" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
+                                        <li className="flex-grow" role="presentation">
+                                            <button className="tab_list_button" id="Basics-tab" data-tabs-target="#Basics" type="button" role="tab" aria-controls="Basics"
+                                                aria-selected="false">Basics</button>
+                                        </li>
+                                        <li className="flex-grow" role="presentation">
+                                            <button className="tab_list_button" id="SFUND-tab" data-tabs-target="#SFUND" type="button" role="tab" aria-controls="SFUND"
+                                                aria-selected="false">SFUND</button>
+                                        </li>
+                                        <li className="flex-grow" role="presentation">
+                                            <button className="tab_list_button" id="SNFTS-tab" data-tabs-target="#SNFTS" type="button" role="tab" aria-controls="SNFTS"
+                                                aria-selected="false">SNFTS</button>
+                                        </li>
+                                        <li className="flex-grow" role="presentation">
+                                            <button className="tab_list_button" id="IGOs-tab" data-tabs-target="#IGOs" type="button" role="tab" aria-controls="IGOs"
+                                                aria-selected="false">IGOs</button>
+                                        </li>
+                                        <li className="flex-grow" role="presentation">
+                                            <button className="tab_list_button" id="INOs-tab" data-tabs-target="#INOs" type="button" role="tab" aria-controls="INOs"
+                                                aria-selected="false">INOs</button>
+                                        </li>
+
+                                        <li className="flex-grow" role="presentation">
+                                            <button className="tab_list_button" id="Blockchain-tab" data-tabs-target="#Blockchain" type="button" role="tab" aria-controls="Blockchain"
+                                                aria-selected="false">Blockchain</button>
+                                        </li>
+                                        <li className="flex-grow" role="presentation">
+                                            <button className="tab_list_button" id="Security-tab" data-tabs-target="#Security" type="button" role="tab" aria-controls="Security"
+                                                aria-selected="false">Security</button>
+                                        </li>
+
+                                        <li className="flex-grow" role="presentation">
+                                            <button className="tab_list_button" id="psf-tab" data-tabs-target="#psf" type="button" role="tab" aria-controls="psf"
+                                                aria-selected="false">Project Selection Framework</button>
+                                        </li>
+                                        <li className="flex-grow" role="presentation">
+                                            <button className="tab_list_button" id="seed-staking-tab" data-tabs-target="#seed-staking" type="button" role="tab" aria-controls="seed-staking"
+                                                aria-selected="false">Seed Staking</button>
+                                        </li>
+                                        <li className="flex-grow" role="presentation">
+                                            <button className="tab_list_button" id="web3-games-tab" data-tabs-target="#web3-games" type="button" role="tab" aria-controls="web3-games"
+                                                aria-selected="false">Web3 Games</button>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className="item_box">
+                        <div id="myTabContent">
+                            <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="Basics" role="tabpanel" aria-labelledby="Basics-tab">
+                                <div id="accordion-collapse" data-accordion="collapse">
+                                    <h2 id="accordion-collapse-heading-1">
+                                        <button type="button" className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                                            <span>What is Flowbite?</span>
+                                            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5 5 1 1 5" />
+                                            </svg>
+                                        </button>
+                                    </h2>
+                                    <div id="accordion-collapse-body-1" className="hidden" aria-labelledby="accordion-collapse-heading-1">
+                                        <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                                            <p className="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
+                                            <p className="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" className="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
+                                        </div>
+                                    </div>
+                                    <h2 id="accordion-collapse-heading-2">
+                                        <button type="button" className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
+                                            <span>Is there a Figma file available?</span>
+                                            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5 5 1 1 5" />
+                                            </svg>
+                                        </button>
+                                    </h2>
+                                    <div id="accordion-collapse-body-2" className="hidden" aria-labelledby="accordion-collapse-heading-2">
+                                        <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+                                            <p className="mb-2 text-gray-500 dark:text-gray-400">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
+                                            <p className="text-gray-500 dark:text-gray-400">Check out the <a href="https://flowbite.com/figma/" className="text-blue-600 dark:text-blue-500 hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
+                                        </div>
+                                    </div>
+                                    <h2 id="accordion-collapse-heading-3">
+                                        <button type="button" className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
+                                            <span>What are the differences between Flowbite and Tailwind UI?</span>
+                                            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5 5 1 1 5" />
+                                            </svg>
+                                        </button>
+                                    </h2>
+                                    <div id="accordion-collapse-body-3" className="hidden" aria-labelledby="accordion-collapse-heading-3">
+                                        <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+                                            <p className="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
+                                            <p className="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
+                                            <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                                            <ul className="pl-5 text-gray-500 list-disc dark:text-gray-400">
+                                                <li><a href="https://flowbite.com/pro/" className="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+                                                <li><a href="https://tailwindui.com/" rel="nofollow" className="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="SFUND" role="tabpanel" aria-labelledby="SFUND-tab">
+                                SFund
+                            </div>
+
+                            <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="SNFTS" role="tabpanel" aria-labelledby="SNFTS-tab">
+
+                            </div>
+
+                            <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="IGOs" role="tabpanel" aria-labelledby="IGOs-tab">
+
+                            </div>
+                            <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="INOs" role="tabpanel" aria-labelledby="INOs-tab">
+
+                            </div>
+
+                            <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="Blockchain" role="tabpanel" aria-labelledby="Blockchain-tab">
+
+                            </div>
+                            <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="Security" role="tabpanel" aria-labelledby="Security-tab">
+
+                            </div>
+                            <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="psf" role="tabpanel" aria-labelledby="psf-tab">
+
+                            </div>
+                            <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="seed-staking" role="tabpanel" aria-labelledby="psf-tab">
+
+                            </div>
+                            <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="web3-games" role="tabpanel" aria-labelledby="psf-tab">
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
